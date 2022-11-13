d273cb524395c4ea684a81bd6f689be1461f990f
Yulu Xu <xuyulu86@126.com>
2022-11-13 17:43:32 +0800

    add sprite demo

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:learning_flame/src/game_widgets/my_game.dart' as game_widgets;
 import 'package:learning_flame/src/router/my_game.dart' as router;
 
 void main() {
-  // final FlameGame myGame = game_widgets.MyGame();
-  final FlameGame myGame = router.MyGame();
+  final FlameGame myGame = game_widgets.MyGame();
+  // final FlameGame myGame = router.MyGame();
   runApp(GameWidget(game: myGame));
 }
--- lib/src/game_widgets/my_game.dart
+++ lib/src/game_widgets/my_game.dart
@@ -1,3 +1,24 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 
-class MyGame extends FlameGame {}
+class MyGame extends FlameGame {
+  @override
+  Future<void>? onLoad() async {
+    await add(MySpriteComponent());
+  }
+}
+
+class MySpriteComponent extends SpriteComponent {
+  MySpriteComponent() : super(size: Vector2.all(80), anchor: Anchor.center);
+
+  @override
+  Future<void>? onLoad() async {
+    sprite = await Sprite.load('crate.png');
+  }
+
+  @override
+  void onGameResize(Vector2 size) {
+    position = size / 2;
+    super.onGameResize(size);
+  }
+}
