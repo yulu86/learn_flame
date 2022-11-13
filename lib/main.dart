bb04361e4e7c50c5256909c8a03dd45a9e9310aa
Yulu Xu <xuyulu86@126.com>
2022-11-13 16:45:09 +0800

    add blank game

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:learning_flame/src/game_widgets/my_game.dart' as game_widgets;
 
 void main() {
-  runApp(const MyApp());
+  final game_widgets.MyGame myGame = game_widgets.MyGame();
+  runApp(GameWidget(game: myGame));
 }
