b998f0c2ffba8bdcb502428957efaa2b6e202709
Yulu Xu <xuyulu86@126.com>
2022-11-13 17:13:39 +0800

    add router demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:learning_flame/src/game_widgets/my_game.dart' as game_widgets;
+import 'package:learning_flame/src/router/my_game.dart' as router;
 
 void main() {
-  final game_widgets.MyGame myGame = game_widgets.MyGame();
+  // final FlameGame myGame = game_widgets.MyGame();
+  final FlameGame myGame = router.MyGame();
   runApp(GameWidget(game: myGame));
 }
