2609289bbb1f86aa64c0d7e8da99fefe95185243
Yulu Xu <xuyulu86@126.com>
2022-11-20 11:57:36 +0800

    add attack demo

--- lib/src/sprites/sprites.dart
+++ lib/src/sprites/sprites.dart
@@ -5,6 +5,67 @@ import 'package:flame/sprite.dart';
 class MyGame extends FlameGame {
   @override
   Future<void>? onLoad() async {
+    _loadGhostSprites();
+    _loadAttackSprites();
+    _loadWalkSprites();
+    return super.onLoad();
+  }
+
+  void _loadAttackSprites() async {
+    final spriteSheet = SpriteSheet(
+      image: await images.load('_Attack.png'),
+      srcSize: Vector2(120, 80),
+    );
+
+    final attackAnimation =
+        spriteSheet.createAnimation(row: 0, stepTime: 0.16, to: 3);
+
+    final spriteSize = Vector2(120, 80);
+
+    final attackAnimationComponent = SpriteAnimationComponent(
+      animation: attackAnimation,
+      position: Vector2(150, 400),
+      size: spriteSize,
+    );
+
+    final boxSpriteSheet = SpriteSheet(
+      image: await images.load('crate.png'),
+      srcSize: Vector2(100, 100),
+    );
+
+    var boxSpriteComponent = SpriteComponent(
+      sprite: boxSpriteSheet.getSprite(0, 0),
+      position: Vector2(250, 430),
+      size: Vector2(60, 60),
+    );
+
+    addAll([
+      boxSpriteComponent,
+      attackAnimationComponent,
+    ]);
+  }
+
+  void _loadWalkSprites() async {
+    final spriteSheet = SpriteSheet(
+      image: await images.load('_CrouchWalk.png'),
+      srcSize: Vector2(120, 80),
+    );
+
+    final walkAnimation =
+        spriteSheet.createAnimation(row: 0, stepTime: 0.1, to: 7);
+
+    final spriteSize = Vector2(120, 80);
+
+    final walkAnimationComponent = SpriteAnimationComponent(
+      animation: walkAnimation,
+      position: Vector2(150, 500),
+      size: spriteSize,
+    );
+
+    add(walkAnimationComponent);
+  }
+
+  void _loadGhostSprites() async {
     final spriteSheet = SpriteSheet(
       image: await images.load('ghost_spritesheet.png'),
       srcSize: Vector2(16, 18),
